1f57cb42f83b42d09c72e02bc668f5f8172fb5ad
ShindeVikas01 <shinde.vikas01@gmail.com>
2023-07-31 23:01:59 +0530

    [Added]:[Vikas] Create Service Class as per Interface

---
+++ src/main/java/com/infinity/productinventoryapp/service/impl/ProductService.java
@@ -0,0 +1,41 @@
+package com.infinity.productinventoryapp.service.impl;
+
+import java.util.List;
+
+import com.infinity.productinventoryapp.entity.ProductEntity;
+import com.infinity.productinventoryapp.model.ProductModel;
+import com.infinity.productinventoryapp.service.IProductService;
+
+public class ProductService implements IProductService {
+
+	@Override
+	public ProductEntity addProduct(ProductModel productModel) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public List<ProductEntity> getAllProduct() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ProductEntity getProductById(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String deleteProduct(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public ProductEntity updateProduct(Integer id) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
