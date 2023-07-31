a791b67a2b5fe30ce4114c9383ea8650938b9c67
ShindeVikas01 <shinde.vikas01@gmail.com>
2023-07-31 22:25:26 +0530

    [Added]:[Vikas] Product Inventory App setup

---
+++ src/main/java/com/infinity/productinventoryapp/ProductInventoryAppApplication.java
@@ -0,0 +1,13 @@
+package com.infinity.productinventoryapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProductInventoryAppApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProductInventoryAppApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/infinity/productinventoryapp/ProductInventoryAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.infinity.productinventoryapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProductInventoryAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
