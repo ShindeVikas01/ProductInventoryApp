bfd26dd5e2cbbbcf75554e5d89b33d5d627c942c
ShindeVikas01 <shinde.vikas01@gmail.com>
2023-07-31 22:56:55 +0530

    [Added]:[Vikas] Create Interface methods as per our api requirement

---
+++ src/main/java/com/infinity/productinventoryapp/service/IProductService.java
@@ -0,0 +1,15 @@
+package com.infinity.productinventoryapp.service;
+
+import java.util.List;
+
+import com.infinity.productinventoryapp.entity.ProductEntity;
+import com.infinity.productinventoryapp.model.ProductModel;
+
+public interface IProductService {
+
+	public ProductEntity addProduct(ProductModel productModel); 
+	public List<ProductEntity> getAllProduct();
+	public ProductEntity getProductById(Integer id);
+	public String deleteProduct(Integer id);
+	public ProductEntity updateProduct(Integer id);
+}
